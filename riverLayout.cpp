53f4a52e423698f6cf6d68862e21412c75d0c2a1
Zakk <zakk@rsdio.com>
2023-12-17 16:26:59 -0500

    Chase hyprland git

--- riverLayout.cpp
+++ riverLayout.cpp
@@ -639,7 +639,7 @@ void CRiverLayout::replaceWindowDataWith(CWindow* from, CWindow* to) {
 
 void CRiverLayout::onEnable() {
     for (auto& w : g_pCompositor->m_vWindows) {
-        if (w->m_bIsFloating || !w->m_bMappedX11 || !w->m_bIsMapped || w->isHidden())
+        if (w->m_bIsFloating ||  !w->m_bIsMapped || w->isHidden())
             continue;
 
         onWindowCreatedTiling(w.get());
